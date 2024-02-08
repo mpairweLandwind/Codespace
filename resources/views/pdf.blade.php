62677f7516be23446d0cb0fc7abd7c881f685150
mpairweLandwind <mpairwelauben22@gmail.com>
2024-02-08 14:22:21 +0300

    latest commit

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -172,7 +172,12 @@
 </div>
 
   
-
+<footer>
+<div class="logo">   
+    <img src="{{ public_path('footer.png') }}" alt="Logo" alt="Logo" style="width:100%; height: auto; ">
+      
+    </div>
+</footer>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
